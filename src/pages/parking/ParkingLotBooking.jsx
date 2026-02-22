da024cc244deb4cd7a1ee70393bc488db56cec5a
Nishad Bhoyar <nishadbhoyar223@gamil.com>
2026-02-22 12:39:16 +0530

    Payment Button

--- src/pages/parking/ParkingLotBooking.jsx
+++ src/pages/parking/ParkingLotBooking.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import { Car, Clock, CreditCard, X, Phone, FileText, ArrowRight, MapPin } from "lucide-react";
-// import PaymentButton from './PaymentButton';
+import PaymentButton from './PaymentButton';
 
 function ParkingLotBooking({ lot, user, onClose }) {
   const navigate = useNavigate();
