69318025ab372080a2f59ae18e3b6a21f79e709a
Nishad Bhoyar <nishadbhoyar223@gamil.com>
2026-02-23 00:24:02 +0530

    fix: force vite to bundle react-is for recharts

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // 🔥 Add this block to force Vite to bundle react-is
+  optimizeDeps: {
+    include: ['react-is']
+  },
+  build: {
+    commonjsOptions: {
+      include: [/react-is/, /node_modules/]
+    }
+  }
 })
